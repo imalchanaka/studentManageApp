ed2bdd460f2d51828da9cf688d39af2b813d40cc
imal chanaka <107134405+imalchanaka@users.noreply.github.com>
2025-02-08 10:41:52 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is imal chanka  nos {count}
+          count is imal chanka  mapa {count}
         </button>
 
 
