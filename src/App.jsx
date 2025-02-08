aadad2922b9fe385779f3d955ba8d0ad94deaf3f
imal chanaka <107134405+imalchanaka@users.noreply.github.com>
2025-02-08 16:25:54 +0530

    Change name

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is imal chanka  mapa {count}
+          count is imal chanka  Samarasinghe mapa {count}
         </button>
 
 
